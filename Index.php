a6a6f353a5ed06fddd52529fa3b2322f1fb415dc
anacarineaguiar <acsiaguiar9@gmail.com>
2023-08-04 20:23:58 -0300

    start

--- Index.php
+++ Index.php
@@ -2,18 +2,27 @@
     <div id="post-bar">
       <img src="public/images/image.svg" alt="profileImage1" id="image1"/> <br>
       <a href="#" id="nickname1">Catloka</a>
-      <input type="text" id="input-post" placeholder="O que te aconteceu?"/>
+      <input type="text" id="input-post" placeholder="O que aconteceu no jogo hoje?"/>
       <a href="#" id="attachment"><img src="public/images/attachment.svg" alt="attachment"/></a>
       <a href="#" id="emotes"><img src="public/images/emoticon.svg" alt="emoticons"/></a> <br>
       <button type="submit">Poste</button>
     </div>
-    <div class="timeline">
+
+    <div class="timeline1">
       <img src="public/images/image.svg" alt="profileImage" class="image2"/> <br>
       <a href="#" class="nickname2">Mini Sombra</a>
-      <h2 class="timeline-post">Perdi uma rank hoje, baixou meu pdl... aaahhh</h2><br>
+      <p class="timeline-post">Perdi uma rank hoje, baixou meu pdl... aaahhh</p><br>
       <a href="#"><img src="public/images/like.svg" alt="like" class="like"/></a>
       <a href="#" class="comments">Comentários</a>
-      <h2 class="date-post">Janeiro 21, 2021</h2>
+      <p class="date-post">Janeiro 21, 2021</p>
     </div>
+</br>
+    <div class="timeline2">
+    <img src="public/images/image.svg" alt="profileImage" class="image2"/> <br>
+      <a href="#" class="nickname3">Noob Poderoso</a>
+      <p class="timeline-post">Oi galera, bora jogar?</p><br>
+      <a href="#"><img src="public/images/like.svg" alt="like" class="like"/></a>
+      <a href="#" class="comments">Comentários</a>
+      <p class="date-post">Janeiro 22, 2021</p>
   </body>
 </html>
